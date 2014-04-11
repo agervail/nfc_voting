9a15b7cb4ba0b63cf51e077258c3178ae6134aa4
Antoine <agervail@ahmes.probayes.net>
2014-04-11 13:35:45 +0200

    writer cleaned

--- server/write_nfc.py
+++ server/write_nfc.py
@@ -9,27 +9,11 @@ def connected(tag):
   print id_num
   return True
 
-'''
-clf = nfc.ContactlessFrontend('usb')
+if len(sys.argv) != 2:
+  print 'Usage : write_nfc.py [id]'
+  exit(0)
 
-tag = clf.connect(rdwr={'on-connect': connected})
-
-
-#if tag.ndef
-'''
 id_num = int(sys.argv[1])
 clf = nfc.ContactlessFrontend('usb')
 tag = clf.connect(rdwr={'on-connect': connected})
-
-#sp = nfc.ndef.TextRecord('Antoine Gervail', language='fr')
-#sp.name = 'name'
-#sp = nfc.ndef.TextRecord(sys.argv[1])
-#sp.name = 'id'
-#import ipdb;ipdb.set_trace()
-#sp2 = nfc.ndef.TextRecord('Grenoble', language='fr')
-
-#tag.ndef.message = nfc.ndef.Message(sp)
-#tag.ndef.message = nfc.ndef.Message(sp, sp2)
-#print nfc.ndef.SmartPosterRecord(tag.ndef.message[0]).pretty()
-
 
